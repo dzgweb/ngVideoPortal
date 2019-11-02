fa6fdd2dc8147c215aaba3c0ce317e7081440146
ÐSergey Dziadevich <developersiteweb@gmail.com>
2019-11-02 11:25:21 +0300

    fixed all spec bugs

--- src/app/courses/components/course/course.component.spec.ts
+++ src/app/courses/components/course/course.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { CourseComponent } from './course.component';
+import { Course } from '../../models';
 
 describe('CourseComponent', () => {
   let component: CourseComponent;
@@ -21,6 +22,16 @@ describe('CourseComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CourseComponent);
     component = fixture.componentInstance;
+    component.course =  {
+      id: 1,
+      title: 'Video Course 1. Name tag',
+      creationDate: '12.09.2018',
+      duration: 88,
+      description: `Learn about where you can find course descriptions, what information they include, how they work, and details
+       about various components of a course description. Course descriptions report information about a university or college's
+       classes. They're published both in course catalogs that outline degree requirements and in course schedules that contain
+       descriptions for all courses offered during a particular semester.`
+    };
     fixture.detectChanges();
   });
 
