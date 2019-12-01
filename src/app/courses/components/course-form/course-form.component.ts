5de4bff8ebb60d4379b79d464e358bd5981802a9
ÐSergey Dziadevich <developersiteweb@gmail.com>
2019-12-01 16:07:50 +0300

    add course page

--- src/app/courses/components/course-form/course-form.component.ts
+++ src/app/courses/components/course-form/course-form.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Course } from '../../models';
+
 @Component({
   selector: 'app-course-form',
   templateUrl: './course-form.component.html',
   styleUrls: ['./course-form.component.scss']
 })
 export class CourseFormComponent implements OnInit {
+  public course: Course;
 
   constructor() { }
 
   ngOnInit() {
+    this.course = new Course();
   }
 
+  cancel() {
+    this.course = new Course();
+  }
+
+  save() {
+    console.log(this.course);
+  }
 }
