39841ae15bf794b81050a157fe9e05105b299aa2
Sergey Dziadevich <S.Dziadevich@refinitiv.com>
2020-01-09 18:31:30 +0300

    add ngrx effects getCourses$, deleteCourse$

--- src/app/core/@ngrx/courses/courses.effects.ts
+++ src/app/core/@ngrx/courses/courses.effects.ts
@@ -3,7 +3,7 @@ import { Action } from '@ngrx/store';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 
 import { of, Observable } from 'rxjs';
-import { mergeMap, map, catchError} from 'rxjs/operators';
+import { mergeMap, map, catchError, pluck} from 'rxjs/operators';
 
 import * as CoursesActions from './courses.action';
 import { CourseService } from '../../../courses';
@@ -18,6 +18,17 @@ export class CoursesEffects {
       .pipe(
         map(courses => CoursesActions.getCoursesSuccess({ courses })),
         catchError(error => of(CoursesActions.getCoursesError({ error })))
+      ))
+    )
+  );
+
+  deleteCourse$: Observable<Action> = createEffect(() => this.actions$.pipe(
+    ofType(CoursesActions.deleteCourse),
+    pluck('course'),
+    mergeMap((course) => this.courseService.deleteCourse(course)
+      .pipe(
+        map(courses => CoursesActions.deleteCourseSuccess({ course })),
+        catchError(error => of(CoursesActions.deleteCourseError({ error })))
       ))
     )
   );
--- src/app/core/@ngrx/courses/courses.reducer.ts
+++ src/app/core/@ngrx/courses/courses.reducer.ts
@@ -1,48 +1,49 @@
 import { Action, createReducer, on } from '@ngrx/store';
 
 import { CoursesState, initialCoursesState } from './courses.state';
-import * as CoursesAction from './courses.action';
+import * as CoursesActions from './courses.action';
 import { ICourse } from '../../../courses';
 
 export const coursesFeatureKey = 'courses';
 
 const reducer = createReducer(
   initialCoursesState,
-  on(CoursesAction.getCourses, state => {
+  on(CoursesActions.getCourses, state => {
     console.log('GET_COURSES action being handled!');
     return {
       ...state,
       loading: true
     };
   }),
-  on(CoursesAction.getCourseSuccess, (state, payload) => {
+  on(CoursesActions.getCoursesSuccess, (state, { courses }) => {
     console.log('GET_COURSES_SUCCESS action being handled!');
+    const data = [...courses];
     return {
       ...state,
+      data,
       loading: false,
-      loaded: false,
-      error: null
+      loaded: true
     };
   }),
-  on(CoursesAction.getCourseError, (state, payload) => {
+  on(CoursesActions.getCoursesError, (state, { error }) => {
     console.log('GET_COURSES_ERROR action being handled!');
     return {
       ...state,
       loading: false,
       loaded: false,
-      error: null
+      error
     };
   }),
 
-  on(CoursesAction.getCourse, state => {
+  on(CoursesActions.getCourse, state => {
     console.log('GET_COURSE action being handled!');
     return { ...state };
   }),
-  on(CoursesAction.createCourse, state => {
+  on(CoursesActions.createCourse, state => {
     console.log('CREATE_COURSE action being handled!');
     return { ...state };
   }),
-  on(CoursesAction.updateCourse, (state, { course }) => {
+  on(CoursesActions.updateCourse, (state, { course }) => {
     console.log('UPDATE_COURSE action being handled!');
 
     const id = course.id;
@@ -59,13 +60,27 @@ const reducer = createReducer(
     };
   }),
 
-  on(CoursesAction.deleteCourse, (state, { course }) => {
+  on(CoursesActions.deleteCourse, state => {
     console.log('DELETE_COURSE action being handled!');
-
+    return { ...state};
+  }),
+  on(CoursesActions.deleteCourseSuccess, (state, { course }) => {
+    console.log('DELETE_COURSE_SUCCESS action being handled!');
     const data = state.data.filter(c => c.id !== course.id);
-
-    return { ...state, data};
+    return {
+      ...state,
+      data
+    };
+  }),
+  on(CoursesActions.deleteCourseError, (state, props) => {
+    console.log('DELETE_COURSE_ERROR action being handled!');
+    const error = props.error;
+    return {
+      ...state,
+      error
+    };
   })
+
 );
 
 export function coursesReducer(state: CoursesState | undefined, action: Action) {
